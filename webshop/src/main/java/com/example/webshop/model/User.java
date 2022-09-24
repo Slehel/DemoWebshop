5f6d5e4cd547d8f4b2e1a099db0a962cff2a0acb
Sultész Lehel <67593@4ig.hu>
2022-09-24 14:38:53 +0200

    Added User List with mock users

---
+++ webshop/src/main/java/com/example/webshop/model/User.java
@@ -0,0 +1,60 @@
+package com.example.webshop.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "type")
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- webshop/src/main/java/com/example/webshop/product/ProductConfig.java
+++ webshop/src/main/java/com/example/webshop/product/ProductConfig.java
@@ -10,27 +10,27 @@ import java.util.List;
 public class ProductConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(ProductRepository repository) {
-        return args -> {
-            Product phone = new Product(
-                    "Phone",
-                    "this is a phone",
-                    5,
-                    0,
-                    "kep.jpg"
-            );
-
-            Product laptop = new Product(
-                    "Laptop",
-                    "this is a laptop",
-                    10,
-                    0,
-                    "kep2.jpg"
-            );
-
-            repository.saveAll(
-                    List.of(phone, laptop)
-            );
-        };
+    CommandLineRunner commandLineRunner(ProductRepository repository) {return null;
+//        ;args -> {
+//            Product phone = new Product(
+//                    "Phone",
+//                    "this is a phone",
+//                    5,
+//                    0,
+//                    "kep.jpg"
+//            );
+//
+//            Product laptop = new Product(
+//                    "Laptop",
+//                    "this is a laptop",
+//                    10,
+//                    0,
+//                    "kep2.jpg"
+//            );
+//
+//            repository.saveAll(
+//                    List.of(phone, laptop)
+//            );
+//        };
     }
 }
