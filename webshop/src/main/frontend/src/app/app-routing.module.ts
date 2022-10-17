d75ad27ef1f5089dfe283c7bd05c4b33e136d23a
Sultész Lehel <67593@4ig.hu>
2022-10-17 14:43:24 +0200

    Removed unused files

--- webshop/src/main/frontend/src/app/app-routing.module.ts
+++ webshop/src/main/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './admin/users/users.component';
 import { ProductsComponent } from './admin/products/products.component';
 import { ShopproductComponent } from './shopproduct/shopproduct.component';
 
 const routes: Routes = [
-{ path: 'admin/users', component: UsersComponent },
 { path: 'admin/products', component: ProductsComponent },
 { path: 'shop', component: ShopproductComponent }
 ];
